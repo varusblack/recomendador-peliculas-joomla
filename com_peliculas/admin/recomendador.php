2d0d4fbf38f82207d05b8ed66f18783c2f15e7ab
varusblack@gmail.com <varusblack@gmail.com@82928036-63f5-a556-2681-9909c439e72a>
2012-04-04 10:59:59 +0000

    Empezando con los famosos

---
+++ com_peliculas/admin/recomendador.php
@@ -0,0 +1,19 @@
+<?php
+	defined('_JEXEC') or die('Restricted access');
+	
+	$controller='';
+	
+	if ($controller = JRequest::getVar('controller')) {
+		require_once (JPATH_COMPONENT_ADMINISTRATOR.DS.'controllers'.DS.$controller.'.php');
+	} else
+		require_once( JPATH_COMPONENT_ADMINISTRATOR.DS.'controller.php' );
+	
+	$component_name='Biblioteca';
+	$classname = $component_name.'Controller'.$controller;
+	$controller = new $classname();
+	
+	$controller->execute(JRequest::getWord('task'));
+
+	$controller->redirect();
+
+?>
