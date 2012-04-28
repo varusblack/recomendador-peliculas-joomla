a66021b6a834225ea108986e5dadc77babfa7b21
jenkin90@gmail.com <jenkin90@gmail.com@82928036-63f5-a556-2681-9909c439e72a>
2012-04-28 18:18:42 +0000

    Creado usuarios

--- com_peliculas/admin/uninstall.sql
+++ com_peliculas/admin/uninstall.sql
@@ -4,3 +4,4 @@ DROP TABLE IF EXISTS `#__famosos`;
 DROP TABLE IF EXISTS `#__categorias`;
 DROP TABLE IF EXISTS `#__actorespelicula`;
 DROP TABLE IF EXISTS `#__peliculas`;
+ALTER TABLE `#__users`  DROP `vector`;
