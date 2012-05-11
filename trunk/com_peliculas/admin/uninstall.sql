1a2ea9753118d1fc961b2d9940859d1da41f3cd2
varusblack@gmail.com <varusblack@gmail.com@82928036-63f5-a556-2681-9909c439e72a>
2012-05-11 10:35:35 +0000

    Importados votos y usuarios

--- trunk/com_peliculas/admin/uninstall.sql
+++ trunk/com_peliculas/admin/uninstall.sql
@@ -1,8 +1,8 @@
-
 DROP TABLE IF EXISTS `#__categoriasPeliculas`;
 DROP TABLE IF EXISTS `#__famosos`;
 DROP TABLE IF EXISTS `#__categorias`;
 DROP TABLE IF EXISTS `#__actorespelicula`;
 DROP TABLE IF EXISTS `#__peliculas`;
+DELETE FROM `#__users`WHERE id>100000;
 ALTER TABLE `#__users`  DROP `vector`;
 DROP TABLE IF EXISTS `#__votos`;
