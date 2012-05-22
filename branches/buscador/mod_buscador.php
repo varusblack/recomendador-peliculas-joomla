47a7eba55dd2419987a56acc9360299286d70ad7
varusblack@gmail.com <varusblack@gmail.com@82928036-63f5-a556-2681-9909c439e72a>
2012-05-22 10:06:14 +0000

    Modulo buscador casi terminado

--- branches/buscador/mod_buscador.php
+++ branches/buscador/mod_buscador.php
@@ -1,13 +1,12 @@
-<?php
-    defined('_JEXEC') or die("acceso denegado");
-	
-	require_once(dirname(__FILE__).DS.'helper.php');
-	
-	$filmCount = $params->get('filmcount');
-	
-	$items = ModJDatabaseHelper::getItems($filmCount);
-	
-	require(JModuleHelper::getLayoutPath('mod_jdatabase'));
-	
-
-?>
+<div class="">
+	<form class="" action="index.php" method="post">
+		<h2>Buscar películas</h2>
+		<input type="text" id="tituloBuscado" name="tituloBuscado"/>
+		<button type="submit">Buscar</button>		
+		
+		<input type="hidden" name="option" value="com_peliculas" />
+	    <input type="hidden" name="controller" value="films" />
+	    <input type="hidden" name="task" value="busquedaRapida" />
+	</form>
+</div>
+<a href="index.php?option=com_peliculas&controller=Films&task=prepararBusquedaAvanzada">Búsqueda avanzada</a>
