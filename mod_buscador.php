9e0e3c6fe6e97abb8415b8387365da6010e74463
varusblack@gmail.com <varusblack@gmail.com@82928036-63f5-a556-2681-9909c439e72a>
2012-05-22 13:25:40 +0000

    El buscador ya es funcional

--- mod_buscador.php
+++ mod_buscador.php
@@ -1,12 +1,11 @@
 <div class="">
-	<form class="" action="index.php" method="post">
+	<form class="form-validate" action="index.php" method="post">
 		<h2>Buscar películas</h2>
 		<input type="text" id="tituloBuscado" name="tituloBuscado"/>
 		<button type="submit">Buscar</button>		
 		
 		<input type="hidden" name="option" value="com_peliculas" />
-	    <input type="hidden" name="controller" value="films" />
 	    <input type="hidden" name="task" value="busquedaRapida" />
 	</form>
 </div>
-<a href="index.php?option=com_peliculas&controller=Films&task=prepararBusquedaAvanzada">Búsqueda avanzada</a>
+<a href="index.php?option=com_peliculas&task=prepararBusquedaAvanzada">Búsqueda avanzada</a>
