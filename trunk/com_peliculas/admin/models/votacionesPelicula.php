72d70c44c566c1c5df004da7bcbcf5bb272c0ccd
varusblack@gmail.com <varusblack@gmail.com@82928036-63f5-a556-2681-9909c439e72a>
2012-05-26 13:15:13 +0000

    intentando paginacion y ordenacion en el front-end

--- trunk/com_peliculas/admin/models/votacionesPelicula.php
+++ trunk/com_peliculas/admin/models/votacionesPelicula.php
@@ -6,7 +6,26 @@ jimport('joomla.application.component.model');
 
 class PeliculasModelVotacionesPelicula extends JModel {
 	
+	var $_total;
+    var $_pagination;
+
+    function __construct() {
+        parent::__construct();
+        global $mainframe, $option;
+        $mainframe = JFactory::getApplication();
+        $limit = $mainframe->getUserStateFromRequest($option.'.peliculas.list.limit', 'limit', $mainframe->getCfg('list_limit'), 'int');
+        $limitstart = JRequest::getVar('limitstart', 0, '', 'int');
+        $this->setState('limit', $limit);
+        $this->setState('limitstart', $limitstart);
+    }
 	
+	function getPagination($resultados) {
+        if (empty($this->_pagination)) {
+            jimport('joomla.html.pagination');
+            $this->_pagination = new JPagination(count($resultados), $this->getState('limitstart'), $this->getState('limit'));
+        }
+        return $this->_pagination;
+    }
 	
 	function votarPelicula ($idUsuario, $idPelicula, $puntuacion) {
 		$timestamp = time();
@@ -70,10 +89,17 @@ class PeliculasModelVotacionesPelicula extends JModel {
         return $db->loadAssoc();
 	}
 	
-	function obtenerPeliculasVotadasPorUsuario ($idUsuario) {
+	function obtenerPeliculasVotadasPorUsuario ($idUsuario,$limites=NULL) {
 		$db = &JFactory::getDbo();
 // 		Devuelve: id,titulo,anno,tituloEspanol,director,puntuacion,timestamp
         $query = "SELECT #__peliculas.id AS id, #__peliculas.titulo AS titulo, #__peliculas.anno AS anno, #__peliculas.tituloEspanol AS tituloEspanol,#__famosos.nombre AS director, #__votos.voto AS puntuacion, #__votos.timestamp AS timestamp FROM #__votos INNER JOIN #__peliculas ON #__votos.idPelicula=#__peliculas.id LEFT JOIN #__famosos ON #__peliculas.idDirector=#__famosos.id WHERE #__votos.idUsuario='{$idUsuario}'";
+        
+		if($limites !=NULL){
+			$start = $this->getState('limitstart');
+       		$limit = $this->getState('limit');
+			$query = $query." ".$this->_getWhereString() . " " . $this->_getOrderString() . " LIMIT $start,$limit";
+		}
+        
         $db->setQuery($query);
         return $db->loadAssocList();
 	}
@@ -82,6 +108,7 @@ class PeliculasModelVotacionesPelicula extends JModel {
 		$db = &JFactory::getDbo();
 // 		Devuelve: id,titulo,anno,tituloEspanol,director
         $query = "SELECT #__peliculas.id AS id, #__peliculas.titulo AS titulo, #__peliculas.anno AS anno, #__peliculas.tituloEspanol AS tituloEspanol, #__famosos.nombre AS director FROM #__peliculas LEFT JOIN #__famosos ON #__famosos.id=#__peliculas.idDirector WHERE #__peliculas.id NOT IN (SELECT idPelicula FROM #__votos WHERE idUsuario='{$idUsuario}') ORDER BY RAND() LIMIT 10";
+        
         $db->setQuery($query);
         return $db->loadAssocList();
 	}
