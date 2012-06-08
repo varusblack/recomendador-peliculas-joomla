218c4e4a2a373ea388f3f582da3a879c6a9c1811
varusblack@gmail.com <varusblack@gmail.com@82928036-63f5-a556-2681-9909c439e72a>
2012-06-08 11:55:39 +0000

    Ya se ve cada pelicula en un recuadro. Insertado en el XML para la instalacion las caratulas de las imagenes

--- trunk/com_peliculas/site/views/votarPeliculas/tmpl/default.php
+++ trunk/com_peliculas/site/views/votarPeliculas/tmpl/default.php
@@ -7,43 +7,56 @@ defined('_JEXEC') or die('Restricted access');
 	if(count($this->peliculas) == 0){
 		echo "No hay películas para votar.";
 	}else{
-		$contador = 1;
 		foreach($this->peliculas as $pelicula){ ?>
-			<div class="leading">
-				<h2 class="contentheading"><?php echo $pelicula["tituloEspanol"]." (".$pelicula["titulo"].")"; ?></h2>
-				<span class="modifydate">Categoría: </span>
-				<?php
-				$idPelicula = $pelicula["id"];
-				$categorias = $this->categoriasPeliculas[$idPelicula];
-				$cadenaCategorias = '';
-				foreach($categorias as $cat){
-					$cadenaCategorias = $cadenaCategorias." ,".$cat["categoria"];
-				}
-				?>
-				<span><?php echo $cadenaCategorias; ?></span>
-				
-				<span class="modifydate">Año: </span>
-				<span><?php echo $pelicula["anno"]; ?></span>
-				
-				<span class="modifydate">Salida a la venta: </span>
-				<span><?php echo $pelicula["videoRelease"]; ?></span>
-				
-				<span class="modifydate">Director: </span>
-				<span><?php echo $pelicula["director"]; ?></span>
-				
-				<span class="modifydate">Puntuación: </span>
-				<select name="<?php echo "puntuacion".$idPelicula; ?>" id="<?php echo "puntuacion".$idPelicula; ?>">
-					<option selected value="no">No la he visto</option>
-					<option value="1.00">1</option>
-					<option value="2.00">2</option>
-					<option value="3.00">3</option>
-					<option value="4.00">4</option>
-					<option value="5.00">5</option>
-				</select>
+			<div class="Post">
+				<div class="Post-tl"></div>
+				<div class="Post-tr"></div>
+				<div class="Post-bl"></div>
+				<div class="Post-br"></div>
+				<div class="Post-tc"></div>
+				<div class="Post-bc"></div>
+				<div class="Post-cl"></div>
+				<div class="Post-cr"></div>
+				<div class="Post-cc"></div>
+				<div class="Post-body"> 
+					<div class="Post-inner">
+						<div class="PostContent">
+							<h2 class="contentheading"><?php echo $pelicula["tituloEspanol"]." (".$pelicula["titulo"].")"; ?></h2>
+							<span class="modifydate">Categoría: </span>
+							<span>
+							<?php
+							$idPelicula = $pelicula["id"];
+							$categorias = $this->categoriasPeliculas[$idPelicula];
+							for ($i=0;$i<count($categorias);$i++){
+								if($i != (count($categorias) - 1)){
+									echo $categorias[$i]["categoria"].", ";
+								}else{
+									echo $categorias[$i]["categoria"];
+								}
+							}
+							?>
+							</span>
+							
+							<span class="modifydate">Año: </span>
+							<span><?php echo $pelicula["anno"]; ?></span>
+							
+							<span class="modifydate">Director: </span>
+							<span><?php echo $pelicula["director"]; ?></span>
+							
+							<span class="modifydate">Puntuación: </span>
+							<select name="<?php echo "puntuacion".$idPelicula; ?>" id="<?php echo "puntuacion".$idPelicula; ?>">
+								<option selected value="no">No la he visto</option>
+								<option value="1.00">1</option>
+								<option value="2.00">2</option>
+								<option value="3.00">3</option>
+								<option value="4.00">4</option>
+								<option value="5.00">5</option>
+							</select>
+						</div>
+					</div>
+				</div>
 			</div>
-			
 	<?php
-			$contador = $contador + 1;
 		}
 	?>
 	<div align="center">
@@ -56,7 +69,6 @@ defined('_JEXEC') or die('Restricted access');
 	
 
 	<input type="hidden" name="option" value="com_peliculas" />
-    <input type="hidden" name="controller" value="films" />
     <input type="hidden" name="task" value="votarMasivo" />
     <input type="hidden" name="identificadores" value="<?php echo base64_encode(serialize($this->identificadores)); ?>" />
 	
