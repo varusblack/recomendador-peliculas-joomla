e1da86f478cb118cc493b7a2181c6042af1ec279
jenkin90@gmail.com <jenkin90@gmail.com@82928036-63f5-a556-2681-9909c439e72a>
2012-06-09 11:29:39 +0000

    Integridad referencial "a lo bruto" hecha

--- com_peliculas/admin/controllers/famosos.php
+++ com_peliculas/admin/controllers/famosos.php
@@ -12,23 +12,23 @@ class PeliculasControllerFamosos extends JController {
     }
 
     function display() {
-    	global $mainframe,$option;
-		
+        global $mainframe, $option;
+
         $modelo = $this->getModel("famosos");
         $famosos = $modelo->obtenerFamososLimites();
 
         $vista = $this->getView("famosos", "html");
         $pagination = $modelo->getPagination();
-		$filter_order=$mainframe->getUserStateFromRequest($option.'.famosos.filter_order', 'filter_order', '', 'word' );
-        $filter_order_Dir=$mainframe->getUserStateFromRequest($option.'.famosos.filter_order_Dir', 'filter_order_Dir', '', 'word' );
-        $filter_state=$mainframe->getUserStateFromRequest($option.'.famosos.filter_state', 'filter_state', '', 'word' );
-        $search=$mainframe->getUserStateFromRequest($option.'.famosos.search', 'search', '', 'word' );
-		
-		$vista->assignRef("filter_order", $filter_order);
+        $filter_order = $mainframe->getUserStateFromRequest($option . '.famosos.filter_order', 'filter_order', '', 'word');
+        $filter_order_Dir = $mainframe->getUserStateFromRequest($option . '.famosos.filter_order_Dir', 'filter_order_Dir', '', 'word');
+        $filter_state = $mainframe->getUserStateFromRequest($option . '.famosos.filter_state', 'filter_state', '', 'word');
+        $search = $mainframe->getUserStateFromRequest($option . '.famosos.search', 'search', '', 'word');
+
+        $vista->assignRef("filter_order", $filter_order);
         $vista->assignRef("filter_order_Dir", $filter_order_Dir);
         $vista->assignRef("filter_state", $filter_state);
         $vista->assignRef("pagination", $pagination);
-		$vista->assignRef("search", $search);
+        $vista->assignRef("search", $search);
         $vista->assignRef("famosos", $famosos);
         $vista->display();
     }
@@ -44,12 +44,12 @@ class PeliculasControllerFamosos extends JController {
     }
 
     function remove() {
-        
+
         $cid = JRequest::getVar('cid', 0, '', 'array');
         $modelo = $this->getModel('famosos');
         $famosos = array();
         foreach ($cid as $idFamoso) {
-            $famoso = $modelo->obtenerFamosoPorId($idFamoso);
+            $famoso = $modelo->obtenerFamosoPorId($idFamoso);       
             $famosos[] = $famoso;
         }
 
@@ -87,21 +87,21 @@ class PeliculasControllerFamosos extends JController {
         $vista = $this->getView('famosos', 'html');
         $vista->add();
     }
-    
+
     function processRemove() {
         $elementsToDelete = JRequest::getVar('elementsToDelete');
         $elementsToDelete = unserialize(base64_decode($elementsToDelete));
-        
+
         $modeloFamosos = $this->getModel('famosos');
-        $modeloActoresPelicula=$this->getModel('actoresPelicula');
-        $modeloPeliculas=$this->getModel('films');
-        
+        $modeloActoresPelicula = $this->getModel('actoresPelicula');
+        $modeloPeliculas = $this->getModel('films');
+
         foreach ($elementsToDelete as $element) {
             $modeloFamosos->borrarFamoso($element["id"]);
             $modeloActoresPelicula->borrarPorFamoso($element["id"]);
             $modeloPeliculas->quitarDirector($element["id"]);
         }
-        $correcto=true;
+        $correcto = true;
         if ($correcto) {
             $aviso = "Se borraron los elementos seleccionados";
         } else {
