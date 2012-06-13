031cf34112560f90925939ded4c6843c9568b398
varusblack@gmail.com <varusblack@gmail.com@82928036-63f5-a556-2681-9909c439e72a>
2012-06-13 19:53:30 +0000

    Puerta de entrada del back end corregida

--- com_peliculas/admin/peliculas.php
+++ com_peliculas/admin/peliculas.php
@@ -2,20 +2,24 @@
 
 defined('_JEXEC') or die('Restricted access');
 
-$controller = '';
+$controller = JRequest::getVar('controller');
 
-
-
-if ($controller == JRequest::getVar('controller')) {
-    require_once (JPATH_COMPONENT_ADMINISTRATOR . DS . 'controllers' . DS . $controller . '.php');
-} else{
-    require_once( JPATH_COMPONENT_ADMINISTRATOR . DS . 'controller.php' );
+if ($controller == ''){
+	require_once (JPATH_COMPONENT_ADMINISTRATOR . DS . 'controllers' . DS . 'default.php');
+	$component_name = 'Peliculas';
+	$classname = $component_name . 'ControllerDefault';
+	$controller = new $classname();
+	$task = 'mostrar';
+}else{
+	require_once (JPATH_COMPONENT_ADMINISTRATOR . DS . 'controllers' . DS . $controller . '.php');
+	$component_name = 'Peliculas';
+	$classname = $component_name . 'Controller' . $controller;
+	$controller = new $classname();
+	$task = JRequest::getWord('task');
 }
-$component_name = 'Peliculas';
-$classname = $component_name . 'Controller' . $controller;
-$controller = new $classname();
 
-$controller->execute(JRequest::getWord('task'));
+$controller->execute($task);
 
 $controller->redirect();
+
 ?>
