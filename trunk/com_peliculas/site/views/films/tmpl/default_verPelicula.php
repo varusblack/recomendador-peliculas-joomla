29fa2492311941b0b0dd46792f781c78d57a7c71
jenkin90@gmail.com <jenkin90@gmail.com@82928036-63f5-a556-2681-9909c439e72a>
2012-06-19 19:48:43 +0000

    css olvidado

--- trunk/com_peliculas/site/views/films/tmpl/default_verPelicula.php
+++ trunk/com_peliculas/site/views/films/tmpl/default_verPelicula.php
@@ -14,56 +14,58 @@ defined('_JEXEC') or die('Restricted access');
     <div class="Post-body">
 	<div class="Post-inner">
 	    <div class="PostContent">
-		<div class="cartel">
-		    <img src="index.php?option=com_peliculas&task=mostrarFoto&id=<?php echo $this->resultado["idPelicula"];?>&tam=200&format=raw">
-		</div>
-		<h3><?php echo $this->resultado["tituloEspanol"] . " (" . $this->resultado["titulo"] . ")" ?></h3>
+		<div class="pelicula">
+		    <div class="cartel">
+			<img src="index.php?option=com_peliculas&task=mostrarFoto&id=<?php echo $this->resultado["idPelicula"]; ?>&tam=300&format=raw">
+		    </div>
+		    <h3><?php echo $this->resultado["tituloEspanol"] . " (" . $this->resultado["titulo"] . ")" ?></h3>
 
-		<div>
-		    <span class="indicador">Año: </span>
-		    <span><?php echo $this->resultado["anno"] ?></span>
-		</div>
+		    <div>
+			<span class="indicador">Año: </span>
+			<span><?php echo $this->resultado["anno"] ?></span>
+		    </div>
 
-		<div>
-		    <span class="indicador">Categorías: </span>
-		    <span><?php echo $this->resultado["categorias"] ?></span>
-		</div>
+		    <div>
+			<span class="indicador">Categorías: </span>
+			<span><?php echo $this->resultado["categorias"] ?></span>
+		    </div>
 
-		<div>
-		    <span class="indicador">Director: </span>
-		    <span><?php echo $this->resultado["director"] ?></span>
-		</div>
+		    <div>
+			<span class="indicador">Director: </span>
+			<span><?php echo $this->resultado["director"] ?></span>
+		    </div>
 
-		<div>
-		    <span class="indicador">Actores: </span>
-		    <span><?php echo $this->resultado["actores"] ?></span>
-		</div>
+		    <div>
+			<span class="indicador">Resumen: </span>
+			<span><?php echo $this->resultado["resumenEspa"] ?></span>
+		    </div>
 
-		<?php if (isset($this->resultado["puntuacion"])) { ?>
-    		<span class="indicador">Puntuación: </span>
-    		<select name="puntuacion" id="puntuacion" onchange="votar(<?php echo $this->resultado["idPelicula"]; ?>,this.value)">
-			<?php if (strcmp($this->resultado["puntuacion"], "no") == 0) { ?>
-			    <option selected="selected" value="no">No la he visto</option>
-			<?php } else {
-			    ?>
-			    <option value="no">No la he visto</option>
-			    <?php
-			}
-
-			for ($i = 1; $i < 6; $i++) {
-			    if (strcmp($i . ".00", $this->resultado["puntuacion"]) == 0) {
-				?>
-	    		    <option selected="selected" value="<?php echo $i . ".00"; ?>"><?php echo $i . ".00"; ?></option>
+		    <?php if (isset($this->resultado["puntuacion"])) { ?>
+    		    <span class="indicador">Puntuación: </span>
+    		    <select name="puntuacion" id="puntuacion" onchange="votar(<?php echo $this->resultado["idPelicula"]; ?>,this.value)">
+			    <?php if (strcmp($this->resultado["puntuacion"], "no") == 0) { ?>
+				<option selected="selected" value="no">No la he visto</option>
 			    <?php } else {
 				?>
-	    		    <option value="<?php echo $i . ".00"; ?>"><?php echo $i . ".00"; ?></option>
+				<option value="no">No la he visto</option>
 				<?php
 			    }
-			}
-			?>
-    		</select>
-		<?php } ?>
 
+			    for ($i = 1; $i < 6; $i++) {
+				if (strcmp($i . ".00", $this->resultado["puntuacion"]) == 0) {
+				    ?>
+	    			<option selected="selected" value="<?php echo $i . ".00"; ?>"><?php echo $i . ".00"; ?></option>
+				<?php } else {
+				    ?>
+	    			<option value="<?php echo $i . ".00"; ?>"><?php echo $i . ".00"; ?></option>
+				    <?php
+				}
+			    }
+			    ?>
+    		    </select>
+		    <?php } ?>
+
+		</div>
 	    </div>
 	</div>
     </div>
